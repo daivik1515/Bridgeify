d3c1d25f1b3e00cfded34b20cc857e9f0a5b5ac3
daivik1515 <daivik.15.saharan@gmail.com>
2025-11-06 17:14:57 -0500

    Added the Mavel Project Files

---
+++ src/main/java/com/jobPortal/Bridgeify/BridgeifyApplication.java
@@ -0,0 +1,13 @@
+package com.jobPortal.Bridgeify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BridgeifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BridgeifyApplication.class, args);
+	}
+
+}
